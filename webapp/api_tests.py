d8fd71d58320595dd444c2d81b2b2f4289147fbb
Conor Gormally <ConorGormally@eduroam-163-19.dyn.carleton.edu>
2018-10-16 23:13:38 -0500

    pushing so Paul can pull

--- webapp/api_tests.py
+++ webapp/api_tests.py
@@ -22,21 +22,21 @@ class api_tests(unittest.TestCase):
         self.string_from_server = self.data_from_server.decode('utf-8')
         self.data = json.loads(self.string_from_server)
         return self.data
-        
-        
+
+
 
     def test_candidates_by_name():
-        self.assertEquals(get_data('candidates?name=Doe'), 
+        self.assertEquals(get_data('candidates?name=Doe'),
               [{'id':'Muleqvse5de', 'first_name':'John', 'last_name':'Doe', 'party':'D', 'state':'AK', 'seat':'S1'}])
-              
+
     def test_candidates_by_party_by_state():
         self.assertEquals(get_data('candidates?state=VA&party=D'), get_data('candidates?name=Maddison'))
 
     def test_candidates_by_state_by_seat():
         self.assertEquals(get_data('candidates?state=VA&seat=17'), get_data('candidates?name=Johnson'))
-        
 
-        
+
+
     def test_pacs_by_name():
         self.assertEquals(get_data('pacs?name=Democratic+Republicans+for+a+New+America'),
                      [{'id':'wsvyhdul2', 'name':'Democratic Republicans for a New America', 'party':'L',
@@ -44,36 +44,31 @@ class api_tests(unittest.TestCase):
 
     def test_pacs_by_party_sensitive():
         self.assertEquals(get_data('pacs?party=D&sensitive=true'), get_data('pacs?id=znkcdot2b'))
-        
+
     def test_pacs_by_industry_by_foreign():
         self.assertEquals(get_data('pacs?industry=Oil+and+Gas&foreign=true'), get_data('pacs?id=h8bmw42by'))
 
 
-        
+
     def test_transactions_to_candidate():
-        self.assertEquals(get_data('trasactions/to_candidate/David'), [{'date': '2017-10-04', 'amount':'25', 
+        self.assertEquals(get_data('trasactions/to_candidate/David'), [{'date': '2017-10-04', 'amount':'25',
                               'contribID': '4alarvf3qvlg', 'contribType': 'indiv', 'recipID': '7f5ozph2h', 'recipType': 'cand'}])
-        
+
     def test_transactions_to_candidate_by_contribType();
-        assertEquals(get_data('transactions/to_candidate/Maddison?contribType=indiv'), get_data('transactions?max_amount = 20&min_amount=0'))
+        self.assertEquals(get_data('transactions/to_candidate/Maddison?contribType=indiv'), get_data('transactions?max_amount = 20&min_amount=0'))
 
     def test_transactions_from_pac():
-        self.assertEquals(get_data('transactions/from_pac/Republicans+For+When+Democrats+Were+Republicans')
-                     get_data('transactions/from_pac?start_date=2017-11-26&end_date=2017-11-28'))
+        self.assertEquals(get_data('transactions/from_pac/Republicans+For+When+Democrats+Were+Republicans'),
+                            get_data('transactions/from_pac?start_date=2017-11-26&end_date=2017-11-28'))
 
-    def transactions_by_date_by_recipType():
+    def test_transactions_by_date_by_recipType():
         self.assertEquals(get_data('transactions?start_date=2017-10-03&end_date=2017-10-09&recipType=pac'),
-        get_data('transactions?max_amount=501&min_amount=499'))
-        
-    def transactions_by_amount():
-        self.assertEquals(get_data('transactions?max_amount=0&min_amount=-400'), [{'date':'2018-06-05', 'amount': -350, 'contribID': 'rm8gcnwothb9',
-                     'contribType': 'indiv', 'recipID':'7f5ozph2h', 'recipType': 'cand'}] )
-        
-    
+                          get_data('transactions?max_amount=501&min_amount=499'))
+
+    def test_transactions_by_amount():
+        self.assertEquals(get_data('transactions?max_amount=0&min_amount=-400'), [{'date':'2018-06-05', 'amount': -350,
+                                   'contribID': 'rm8gcnwothb9', 'contribType': 'indiv', 'recipID':'7f5ozph2h', 'recipType': 'cand'}] )
 
 
     def test_invalid_amount_type():
         self.assertEquals(get_data('transactions?max_amount=f+to+pay+respects'), 'Input Error: Min amount must be number')
-
-
-

a563e5d76942f1197b21905850a700931b83c31a
shta2 <Joesayshi19>
2018-10-28 16:38:56 -0500

    started working on webapp

---
+++ webapp/test.js
@@ -0,0 +1,4 @@
+function candidate_search(){
+    var resultsTableElement = document.getElementById('results_table');
+    resultsTableElement.innerHTML = "you submitted a thing!!";
+}
---
+++ webapp/website.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+import flask
+import json
+
+app = flask.Flask(__name__, static_folder='static', template_folder='templates')
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print('Usage: {0} host port api-port'.format(sys.argv[0]), file=sys.stderr)
+        exit()
+
+    host = sys.argv[1]
+    port = sys.argv[2]
+    api_port = sys.argv[3]
+    app.run(host=host, port=int(port), debug=True)
+    
+    
+@app.route('/')
+def get_main():
+    global api_port
+    return flask.render_template('test.html', api_port=api_port)
